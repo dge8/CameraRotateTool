4640229130924eaeba1dc1ef0754babed7cee799
Dan George <dgeor8@gmail.com>
2021-03-10 18:45:15 +1100

    Initial commit, version 0.1.0

---
+++ CameraRotateTool.ps1
@@ -0,0 +1,157 @@
+﻿# 
+# CameraRotateTool.ps1 v0.1.0
+# www.github.com/dge8/CameraRotateTool
+# 
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2021 Dan George <dgeor8@gmail.com>. All rights reserved.
+# This software is provided with NO WARRANTY WHATSOEVER.
+# 
+# More information about the FSSensorOrientation registry key in Microsoft docs here:
+# https://docs.microsoft.com/en-us/windows-hardware/drivers/stream/camera-device-orientation
+# 
+
+# Builtins, KSCATEGORY GUIDs from 
+# https://docs.microsoft.com/en-us/windows-hardware/drivers/install/kscategory-video
+$RegDeviceClassesRoot = 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\DeviceClasses\'
+$KsCategories   = @(('KSCATEGORY_VIDEO',         '{6994AD05-93EF-11D0-A3CC-00A0C9223196}'),
+                    ('KSCATEGORY_VIDEO_CAMERA',  '{E5323777-F976-4f5b-9B55-B94699C46E44}'),
+                    ('KSCATEGORY_SENSOR_CAMERA', '{24E552D7-6523-47F7-A647-D3465BF1F5CA}'),
+                    ('KSCATEGORY_CAPTURE',       '{65E8773D-8F56-11D0-A3B9-00A0C9223196}'))
+
+# Get configured cameras and current orientation information from the registry
+function Get-Camera {
+    param ()
+    $PnpDevices = Get-PnpDevice |
+        Where-Object { 'Image','Camera' -contains $_.Class } |
+        Select-Object FriendlyName,InstanceId,Status
+    $i = 0
+    $Cameras = @()
+    ForEach ($PnpDevice in $PnpDevices) {
+        $DeviceRegistryId = '##?#' + ($PnpDevice.InstanceId -replace '\\','#') + '#'
+        $SubDeviceKsCategories = @{}
+        ForEach ($KsCategory in $KsCategories) {
+            $devicePath = $regDeviceClassesRoot + $ksCategory[1] + '\' + $DeviceRegistryId + $ksCategory[1]
+            If (-not (Test-Path -LiteralPath $devicePath)) { Continue }
+            $subDevicePaths = (Get-ChildItem -LiteralPath $devicePath).Name
+            ForEach ($subDevicePath in $subDevicePaths) {
+                If ( -not (Test-Path -LiteralPath ('Registry::' + $subDevicePath + "\Properties")) -or 
+                    -not (Test-Path -LiteralPath ('Registry::' + $subDevicePath + "\Device Parameters")) ) {
+                    Continue
+                }
+                $subDeviceRegistryId = Split-Path -Path $subDevicePath -Leaf
+                If ( $subDeviceKsCategories.ContainsKey($subDeviceRegistryId) ) {
+                    $subDeviceKsCategories[$subDeviceRegistryId] += $ksCategory[0]
+                } Else {
+                    $subDeviceKsCategories[$subDeviceRegistryId] = @($ksCategory[0])
+                }
+            }
+        }
+        $SubDeviceKsCategories.GetEnumerator() | ForEach-Object {
+            $SubDeviceNames = @()
+            $FsSensorOrientations = @()
+            ForEach ($KsCategoryName in $_.Value) {
+                $KsCategoryId = ($KsCategories | Where {$_[0] -eq $KsCategoryName})[1]
+                $SubDeviceParameterPath = $RegDeviceClassesRoot + $KsCategoryId + '\' + $DeviceRegistryId + $KsCategoryId + '\' + $_.Name + '\Device Parameters'
+                Try {
+                    $name = Get-ItemPropertyValue -LiteralPath $SubDeviceParameterPath -Name 'FriendlyName'
+                    If ($SubDeviceNames -notcontains $name) { $SubDeviceNames += $name }
+                } Catch { }
+                Try {
+                    $fsso = Get-ItemPropertyValue -LiteralPath $SubDeviceParameterPath -Name 'FSSensorOrientation'
+                    If ($FsSensorOrientations -notcontains $fsso) { $FsSensorOrientations += $fsso }
+                } Catch { }
+            }
+            If ($FsSensorOrientations.Count -eq 0) { $FsSensorOrientations += '0' }
+            $i += 1
+            $Cameras += [PSCustomObject]@{
+                Id                  = $i
+                Device              = $PnpDevice.FriendlyName
+                Name                = $SubDeviceNames -join '/'
+                Status              = $PnpDevice.Status
+                Orientation         = $FsSensorOrientations -join '/'
+                DeviceInstanceId    = $PnpDevice.InstanceId
+                KsCategories        = $_.Value
+                DeviceRegistryId    = $DeviceRegistryId
+                SubDeviceRegistryId = $_.Name
+            }
+        }
+    }
+    $Cameras
+}
+
+# Set orientation of a camera
+Function Set-CameraOrientation {
+    Param(
+        [PSCustomObject]$Camera,
+        [String]$NewOrientation
+    )
+    If ('0','90','180','270' -notcontains $NewOrientation) { Throw "Orientation must be one of [0,90,180,270]." }
+    ForEach ($KsCategoryName in $Camera.KsCategories) {
+        $KsCategoryId = ($KsCategories | Where {$_[0] -eq $KsCategoryName})[1]
+        $DeviceParameterPath = $RegDeviceClassesRoot + $KsCategoryId + '\' + $Camera.DeviceRegistryId + $KsCategoryId + '\' + $Camera.SubDeviceRegistryId + '\Device Parameters'
+        If ($NewOrientation -eq '0') {
+            Remove-ItemProperty -Path $DeviceParameterPath -Name 'FSSensorOrientation' -Force | Out-Null
+        } Else {
+            Set-ItemProperty -Path $DeviceParameterPath -Name 'FSSensorOrientation' -Value $NewOrientation -Type DWord -Force | Out-Null
+        }
+    }
+}
+
+# Check if running at least Windows 10 1607 and PowerShell 5.0
+If ( [Environment]::OSVersion.Version.Major -ne 10 -or 
+    [Environment]::OSVersion.Version.Build -lt 14393 -or
+    $Host.Version.Major -lt 5 ) {
+    Write-Host "Error: CameraRotateTool requires at least Windows 10 1607 and PowerShell 5.0" -ForegroundColor Red
+    Write-Host ''
+    Read-Host -Prompt "Press Enter to exit"
+}
+
+# Elevate to admin privileges if not already
+If ( -not (New-Object Security.Principal.WindowsPrincipal(
+    [Security.Principal.WindowsIdentity]::GetCurrent()
+    )).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        $process = New-Object System.Diagnostics.ProcessStartInfo "PowerShell"
+        $process.Arguments = $MyInvocation.MyCommand.Definition
+        $process.Verb = "runas"
+        [System.Diagnostics.Process]::Start($process) | Out-Null
+        Exit
+    }
+
+# Main interactive loop
+While ($true) {
+
+    Clear-Host
+    Write-Host ''
+    Write-Host '================ CameraRotateTool v0.1.0 ================' -ForegroundColor Cyan
+    Write-Host ''
+
+    Write-Host 'Looking for cameras...'
+    Write-Host ''
+    $Cameras = Get-Camera    
+    
+    If (($Cameras | Measure-Object).Count -eq 0) {
+        Write-Host "Error: Could not identify any compatible connected cameras or imaging devices." -ForegroundColor Red
+        Write-Host ''
+        Read-Host -Prompt "Press Enter to exit"
+        Exit
+    }
+    
+    Write-Host 'Identified cameras: '
+    $Cameras | Select-Object Id,Device,Name,Status,Orientation | Format-Table
+    
+    Do {
+        $Selection = Read-Host -Prompt 'Enter a camera ID to change the orientation, or Q to quit'
+    } Until ($Selection -eq 'q' -or $Cameras.Id -contains $Selection)
+    If ($Selection -eq 'q') { Exit }
+    
+    $CameraId = $Selection
+    Do {
+        $Selection = Read-Host -Prompt 'Enter the new orientation [90,180,270] or 0 to remove the orientation'
+    } Until ('','0','90','180','270' -contains $Selection)
+    If ($Selection -eq '') { Continue }
+
+    $NewOrientation = $Selection
+    $Camera = $Cameras | Where Id -eq $CameraId
+    Set-CameraOrientation $Camera $NewOrientation
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# camerarotatetool
-An interactive Powershell script for rotating UVC webcam feeds on Windows 10
+# CameraRotateTool
+
+## What is CameraRotateTool?
+CameraRotateTool is an unimaginatively named interactive PowerShell script for rotating UVC video feeds on Windows 10. It works by setting keys in the registry that tell the Windows camera pipeline to rotate the camera feed, and so should work for the vast majority of modern Windows apps without requiring any running software in the background.
+
+## How do I use it?
+Download the latest release file on the right, and Right-Click > Run with PowerShell. It should automatically request escalation to admin privileges.
+
+## Will it work for me?
+Maybe. It's had some testing on different platforms, but should still be treated as beta software.
+
+It's been tested with:
+- Logitech BRIO 4K
+- Logitech Rally
+- Microsoft Surface front & rear cameras
+with the following software:
+- Windows Camera app
+- Zoom
+on Windows 10 2004 and 20H2 64-bit.
+
+If it doesn't work for you, feel free to create a new issue. There may be e.g. KSCATEGORYs that are used by different cameras/software that have been missed.
+
+## Can I rotate my camera without using this script?
+Yes, you need to create one or more FSSensorOrientation registry keys as per [this Microsoft doc](https://docs.microsoft.com/en-us/windows-hardware/drivers/stream/camera-device-orientation). The FSSensorOrientation registry key is a DWORD value with decimal 0, 90, 180, or 270 corresponding to the camera correction. Deleting the key also corresponds to 0 degrees.
+
+Start by navigating to HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\DeviceClasses\ in the Registry Editor. Look inside the following keys in turn (your camera may not appear in all of them):
+- `{6994AD05-93EF-11D0-A3CC-00A0C9223196}` (corresponding to KSCATEGORY_VIDEO)
+- `{E5323777-F976-4f5b-9B55-B94699C46E44}` (corresponding to KSCATEGORY_VIDEO_CAMERA)
+- `{24E552D7-6523-47F7-A647-D3465BF1F5CA}` (corresponding to KSCATEGORY_SENSOR_CAMERA, this will only be if your camera has a sensor/IR array, e.g. for Windows Hello)
+- `{65E8773D-8F56-11D0-A3B9-00A0C9223196}` (corresponding to KSCATEGORY_CAPTURE)      
+
+Look through subkeys (two levels) of these to find the 'Device Parameters' key containing a FriendlyName with your camera name. If you have multiple subkeys with the same FriendlyName you can instead match the path to the InstanceID of your camera in the Device Manager. Create the FSSensorOrientation key adjacent to the FriendlyName key. Then repeat for the other KSCATEGORY keys above.
+
+## Roadmap and Contributions
+CameraRotateTool is already essentially feature-complete - but if you know of other registry keys that cause Windows to do useful things to the camera pipeline please feel free to suggest them in an issue or via email.
+
+## License
+CameraRotateTool is released under the MIT license, see LICENSE.
